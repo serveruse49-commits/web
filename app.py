850a952938d0f9eb1e2919fa238c86597b0b82cf
serveruse49-commits <serveruse49@gmail.com>
2026-02-21 18:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,5 +91,8 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port = 4005)
+
+    port = int(os.environ.get("PORT"))
+    app.run(host="0.0.0.0", port=port)
+
 
